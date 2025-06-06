36324069483a12917840c25f5ab334b1130592e2
Т̷е̷н̷ьSAQM <kurichich5@gmail.com>
2025-06-06 17:56:12 +0200

    Update and rename heroku_security.py to shadow_security.py

--- hikka/modules/shadow_security.py
+++ hikka/modules/shadow_security.py
@@ -33,10 +33,10 @@ from ..security import (
 
 
 @loader.tds
-class HerokuSecurityMod(loader.Module):
+class ShadowSecurityMod(loader.Module):
     """Control security settings"""
 
-    strings = {"name": "HerokuSecurity"}
+    strings = {"name": "ShadowSecurity"}
 
     async def client_ready(self):
         self._sgroups: typing.Iterable[str, SecurityGroup] = self.pointer(
