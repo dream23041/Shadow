d491c4d8d4e3f2e99beca2c9853fbed991746f83
Т̷е̷н̷ьSAQM <kurichich5@gmail.com>
2025-06-06 20:10:20 +0200

    Update test.py

--- hikka/modules/test.py
+++ hikka/modules/test.py
@@ -47,11 +47,11 @@ class TestMod(loader.Module):
                 False,
                 (
                     "⚠️ Do not touch, if you don't know what it does!\nBy default, "
-                    " Heroku will try to determine, which client caused logs. E.g. there"
+                    " Shadow will try to determine, which client caused logs. E.g. there"
                     " is a module TestModule installed on Client1 and TestModule2 on"
                     " Client2. By default, Client2 will get logs from TestModule2, and"
                     " Client1 will get logs from TestModule. If this option is enabled,"
-                    " Heroku will send all logs to Client1 and Client2, even if it is"
+                    " Shadow will send all logs to Client1 and Client2, even if it is"
                     " not the one that caused the log."
                 ),
                 validator=loader.validators.Boolean(),
@@ -315,7 +315,7 @@ class TestMod(loader.Module):
         logs = self.lookup("evaluator").censor(logs)
 
         logs = BytesIO(logs.encode("utf-16"))
-        logs.name = "heroku-logs.txt"
+        logs.name = "shadow-logs.txt"
 
         ghash = utils.get_git_hash()
 
@@ -380,11 +380,11 @@ class TestMod(loader.Module):
     async def client_ready(self):
         chat, _ = await utils.asset_channel(
             self._client,
-            "heroku-logs",
-            "🪐 Your Heroku logs will appear in this chat",
+            "shadow-logs",
+            "👻 Your Shadow logs will appear in this chat",
             silent=True,
             invite_bot=True,
-            avatar=" https://raw.githubusercontent.com/coddrago/Heroku/refs/heads/master/assets/heroku-logs.png",
+            avatar=" https://raw.githubusercontent.com/coddrago/Heroku/refs/heads/master/assets/shadow-logs.png",
         )
 
         self.logchat = int(f"-100{chat.id}")
